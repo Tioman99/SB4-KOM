0c61d24a42870528190dd47609e0a3b5a38c40b2
Tioman99 <90039319+Tioman99@users.noreply.github.com>
2023-05-17 18:19:25 +0200

    Fixed GameInputProcessor switch case

--- AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/managers/GameInputProcessor.java
+++ AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/managers/GameInputProcessor.java
@@ -4,33 +4,41 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputAdapter;
 
 public class GameInputProcessor extends InputAdapter { // Changed to switch cases
-	
+
 	public boolean keyDown(int k) {
 		switch (k) {
 			case Keys.UP: {
 				GameKeys.setKey(GameKeys.UP, true);
+				break;
 			}
 			case Keys.LEFT: {
 				GameKeys.setKey(GameKeys.LEFT, true);
+				break;
 			}
 			case Keys.DOWN: {
 				GameKeys.setKey(GameKeys.DOWN, true);
+				break;
 			}
 			case Keys.RIGHT: {
 				GameKeys.setKey(GameKeys.RIGHT, true);
+				break;
 			}
 			case Keys.ENTER: {
 				GameKeys.setKey(GameKeys.ENTER, true);
+				break;
 			}
 			case Keys.ESCAPE: {
 				GameKeys.setKey(GameKeys.ESCAPE, true);
+				break;
 			}
 			case Keys.SPACE: {
 				GameKeys.setKey(GameKeys.SPACE, true);
+				break;
 			}
 			case Keys.SHIFT_LEFT:
 			case Keys.SHIFT_RIGHT: {
 				GameKeys.setKey(GameKeys.SHIFT, true); // This has OR functionality
+				break;
 			}
 		}
 		return true;
@@ -40,36 +48,45 @@ public class GameInputProcessor extends InputAdapter { // Changed to switch case
 		switch (k) {
 			case Keys.UP: {
 				GameKeys.setKey(GameKeys.UP, false);
+				break;
 			}
 			case Keys.LEFT: {
 				GameKeys.setKey(GameKeys.LEFT, false);
+				break;
 			}
 			case Keys.DOWN: {
 				GameKeys.setKey(GameKeys.DOWN, false);
+				break;
 			}
 			case Keys.RIGHT: {
 				GameKeys.setKey(GameKeys.RIGHT, false);
+				break;
 			}
 			case Keys.ENTER: {
 				GameKeys.setKey(GameKeys.ENTER, false);
+				break;
 			}
 			case Keys.ESCAPE: {
 				GameKeys.setKey(GameKeys.ESCAPE, false);
+				break;
 			}
 			case Keys.SPACE: {
 				GameKeys.setKey(GameKeys.SPACE, false);
+				break;
 			}
 			case Keys.SHIFT_LEFT: {
 				switch (k) {
 					case Keys.SHIFT_RIGHT: {
 						GameKeys.setKey(GameKeys.SHIFT, false); // This has AND functionality, I know its ugly
+						break;
 					}
 				}
+				break;
 			}
 		}
 		return true;
 	}
-
+}
 	/*
 	public boolean keyDown(int k) {
 		if(k == Keys.UP) {
@@ -128,8 +145,6 @@ public class GameInputProcessor extends InputAdapter { // Changed to switch case
 		return true;
 	}
 	*/
-	
-}
 
 
 

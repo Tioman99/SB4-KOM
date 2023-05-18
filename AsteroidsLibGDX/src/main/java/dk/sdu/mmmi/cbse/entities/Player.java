419c40b6411afbb035b4904d9b2c3e8243115af5
Tioman99 <90039319+Tioman99@users.noreply.github.com>
2023-05-18 22:12:39 +0200

    Cool  ass flame
    Added my own version of the flame

--- AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/entities/Player.java
+++ AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/entities/Player.java
@@ -7,6 +7,9 @@ import dk.sdu.mmmi.cbse.main.Game;
 
 public class Player extends SpaceObject {
 
+    private float[] flamex;
+    private float[] flamey;
+    private float accelerationTime;
     private boolean left;
     private boolean right;
     private boolean up;
@@ -26,6 +29,8 @@ public class Player extends SpaceObject {
 
         shapex = new float[4]; // polygon will have 4 points
         shapey = new float[4];
+        flamex = new float[3];
+        flamey = new float[3];
 
         radians = valuePi / 2;
         rotationSpeed = 3;
@@ -46,6 +51,17 @@ public class Player extends SpaceObject {
         shapey[3] = y + MathUtils.sin(radians + 4 * valuePi / 5) * 8;
     }
 
+    private void setFlame() {
+        flamex[0] = x + MathUtils.cos(radians - 5 * valuePi / 6) * 6;
+        flamey[0] = y + MathUtils.sin(radians - 5 * valuePi / 6) * 6;
+
+        flamex[1] = x + MathUtils.cos(radians + 5 * valuePi / 6) * 6;
+        flamey[1] = y + MathUtils.sin(radians + 5 * valuePi / 6) * 6;
+
+        flamex[2] = x + MathUtils.cos(radians - valuePi) * (6 + accelerationTime * 50);
+        flamey[2] = y + MathUtils.sin(radians - valuePi) * (6 + accelerationTime * 50);
+    }
+
     public void setLeft(boolean b) {
         left = b;
     }
@@ -60,17 +76,28 @@ public class Player extends SpaceObject {
 
     public void update(float dt) {
 
+        // I have moved setShape call up closer to the setFlame call to minimize the visual delay
+        setShape();
+        
+        // accelerating
+        if (up) {
+            dx += MathUtils.cos(radians) * acceleration * dt;
+            dy += MathUtils.sin(radians) * acceleration * dt;
+            // I prefer having only one if(up) statement, thus setFlame is called here
+            setFlame();
+            accelerationTime += dt;
+            if (accelerationTime > 0.2f) {
+                accelerationTime = 0.1f;
+            }
+        } else {
+            accelerationTime = 0;
+        }
+
         // turning
         if (left) {
             radians += rotationSpeed * dt;
         } else if (right) {
             radians -= rotationSpeed * dt;
-        }
-
-        // accelerating
-        if (up) {
-            dx += MathUtils.cos(radians) * acceleration * dt;
-            dy += MathUtils.sin(radians) * acceleration * dt;
         }
 
         // speed vec
@@ -92,9 +119,6 @@ public class Player extends SpaceObject {
         x += dx * dt;
         y += dy * dt;
 
-        // set shape, re-calculates player shape
-        setShape();
-
         // screen wrap
         wrap();
 
@@ -106,10 +130,24 @@ public class Player extends SpaceObject {
 
         sr.begin(ShapeType.Line);
 
+        // draw ship
         for (int i = 0, j = shapex.length - 1; i < shapex.length; j = i++) {
 
-            sr.line(shapex[i], shapey[i], shapex[j], shapey[j]); // draw line between two points of two arrays
+            sr.line(shapex[i], shapey[i], shapex[j], shapey[j]);
+            // draw a line between two points of two arrays with consecutive indexes i and j.
 
+        }
+
+        // draw flame
+
+        sr.setColor(0, 1, 1, 1);
+
+        if (up) {
+            for (int i =0, j = flamex.length - 1; i < flamex.length; j = i++) {
+
+                sr.line(flamex[i], flamey[i], flamex[j], flamey[j]);
+
+            }
         }
 
         sr.end();
